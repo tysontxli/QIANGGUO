c16cba637b12ab340a5a5d3147d2560fb0753725
chinaltx <chinamen520@qq.com>
2019-02-06 16:45:03 +0800

    linux下定时维护文章数据库的脚本

--- linux_crontab/scripts/QGNews_maintain.sh
+++ linux_crontab/scripts/QGNews_maintain.sh
@@ -3,13 +3,15 @@ everydayLog=$baseLogdir/$(date +%F)/
 [ ! -d "$everydayLog" ] && mkdir -p $everydayLog
 #判断每日的日志文件存放的目录是否存在
 
-echo $everydayLog
-python /root/QIANGGUO/QIANGGUO/scrapy/news_scrapy.py >$everydayLog/$(date +%F_%H:%M).log 2>&1
+nowtime=`date +%F_%H:%M`
+#多条程序的执行时间是有时间差的，为了避免出现push命令执行时已经到了第二分钟，程序需要一个确定的时间
+
+python /root/QIANGGUO/QIANGGUO/scrapy/news_scrapy.py >$everydayLog/${nowtime}.log 2>&1
 #创建每次维护的记录
 
 git init
-git add -f $everydayLog/$(date +%F_%H:%M).log
-git commit -m "这是$(date +%F_%H:%M)的维护日志，由crontab程序自动创建"
+git add -f $everydayLog/${nowtime}.log
+git commit -m "这是${nowtime}的维护日志，由crontab程序自动创建"
 git remote add origin git@github.com:chinaltx/QIANGGUO.git
 git pull origin master
 git push -u origin master
